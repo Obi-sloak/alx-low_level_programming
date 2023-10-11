0bb3e6696d6d83bbefd68a855ecb9fefdf799317
Obi-sloak <odoemeneobinna@gmail.com>
2023-10-11 13:34:07 +0100

    3-main.c,3-calc.h

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,20 +1,44 @@
-#ifndef CALC_H
-#define CALC_H
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
 /**
- * struct op - Struct op
+ * main - Entry point
+ * @argc: Number of arguments
+ * @argv: Array of argument strings
  *
- * @op: The operator
- * @f: The function associated
+ * Return: Always 0 (Success)
  */
-typedef struct op
+int main(int argc, char *argv[])
 {
-	char *op;
-	int (*f)(int a, int b);
-} op_t;
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
-#endif
+	int num1, num2;
+	int result;
+	int (*op_func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op_func = get_op_func(argv[2]);
+
+	if (!op_func)
+	{
+		printf("Error\n");
+		return (99);
+	}
+
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
+
+	result = op_func(num1, num2);
+	printf("%d\n", result);
+
+	return (0);
+}
