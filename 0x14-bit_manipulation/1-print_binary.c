b3ef4a63192f996cef6f2e9ca256e9571d439a09
Obi-sloak <odoemeneobinna@gmail.com>
2023-10-27 01:07:19 +0100

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,7 +7,7 @@
  */
 void print_binary(unsigned long int n)
 {
-	int i, num = 0;
+	int i, number = 0;
 	unsigned long int new;
 
 	for (i = 63; i >= 0; i--)
@@ -17,11 +17,11 @@ void print_binary(unsigned long int n)
 		if (new & 1)
 		{
 			_putchar('1');
-			new++;
+			number++;
 		}
-		else if (num)
+		else if (number)
 			_putchar('0');
 	}
-	if (!num)
+	if (!number)
 		_putchar('0');
 }
