b417d754f5f917542ab3060c6df08592d2bfbfc4
Obi-sloak <odoemeneobinna@gmail.com>
2023-10-30 19:17:11 +0000

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,25 +14,48 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t file, fread, fwrite;
-    char *totalSize;
+    int file, fread, fwrite;
+    char *buffer;
 
-    totalSize = malloc(sizeof(char) * letters);
-    if (totalSize == NULL)
+    // Allocate memory for the buffer
+    buffer = malloc(sizeof(char) * letters);
+    if (buffer == NULL)
         return (0);
+
     if (filename == NULL)
+    {
+        free(buffer);
         return (0);
+    }
 
+    // Open the file
     file = open(filename, O_RDONLY);
     if (file == -1)
+    {
+        free(buffer);
         return (0);
-    fread = read(file, totalSize, letters);
+    }
+
+    // Read from the file
+    fread = read(file, buffer, letters);
     if (fread == -1)
+    {
+        free(buffer);
+        close(file);
         return (0);
-    fwrite = write(STDOUT_FILENO, totalSize, fread);
+    }
+
+    // Write to the standard output
+    fwrite = write(STDOUT_FILENO, buffer, fread);
     if (fwrite == -1)
+    {
+        free(buffer);
+        close(file);
         return (0);
+    }
+
+    // Clean up and return the number of bytes written
+    free(buffer);
     close(file);
-    free(totalSize);
     return (fwrite);
 }
