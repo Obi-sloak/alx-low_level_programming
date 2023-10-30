8a5994ac3ae92cdb6352bcbe0c1429ec61cfd780
Obi-sloak <odoemeneobinna@gmail.com>
2023-10-30 19:07:39 +0000

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,44 +1,38 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include "main.h"
 
 /**
- * read_textfile - Reads a text file and prints it to POSIX stdout.
- * @filename: A pointer to the name of the file.
- * @letters: The number of letters the
- *           function should read and print.
- *
- * Return: If the function fails or filename is NULL - 0.
- *         Otherwise - the actual number of bytes the function can read and print.
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: pointer to text in a file
+ * @letters: number of letters
+ * Return: the actual number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t file_descriptor, bytes_read, bytes_written;
-    char *buffer;
+    ssize_t file, fread, fwrite;
+    char *totalSize;
 
+    totalSize = malloc(sizeof(char) * letters);
+    if (totalSize == NULL)
+        return (0);
     if (filename == NULL)
         return (0);
 
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
+    file = open(filename, O_RDONLY);
+    if (file == -1)
         return (0);
-
-    file_descriptor = open(filename, O_RDONLY);
-    bytes_read = read(file_descriptor, buffer, letters);
-    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-
-    if (file_descriptor == -1 || bytes_read == -1 || bytes_written == -1 || bytes_written != bytes_read)
-    {
-        free(buffer);
+    fread = read(file, totalSize, letters);
+    if (fread == -1)
         return (0);
-    }
-
-    free(buffer);
-    close(file_descriptor);
-
-    return (bytes_written);
+    fwrite = write(STDOUT_FILENO, totalSize, fread);
+    if (fwrite == -1)
+        return (0);
+    close(file);
+    free(totalSize);
+    return (fwrite);
 }
