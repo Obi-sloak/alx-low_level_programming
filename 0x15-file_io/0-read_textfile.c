b3570d9dadf247124dae0c1dc2b723d4527c7854
Obi-sloak <odoemeneobinna@gmail.com>
2023-10-30 19:26:44 +0000

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,55 +7,51 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: pointer to text in a file
- * @letters: number of letters
- * Return: the actual number of letters it could read and print
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the
+ *           function should read and print.
+ *
+ * Return: If the function fails or filename is NULL - 0.
+ *         Otherwise - the actual number of bytes the function can read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int file, fread, fwrite;
-    char *buffer;
+	int file, fread, fwrite;
+	char *buffer;
 
-    // Allocate memory for the buffer
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-        return (0);
+	if (filename == NULL)
+		return (0);
 
-    if (filename == NULL)
-    {
-        free(buffer);
-        return (0);
-    }
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
 
-    // Open the file
-    file = open(filename, O_RDONLY);
-    if (file == -1)
-    {
-        free(buffer);
-        return (0);
-    }
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+	{
+		free(buffer);
+		return (0);
+	}
 
-    // Read from the file
-    fread = read(file, buffer, letters);
-    if (fread == -1)
-    {
-        free(buffer);
-        close(file);
-        return (0);
-    }
+	fread = read(file, buffer, letters);
+	if (fread == -1)
+	{
+		free(buffer);
+		close(file);
+		return (0);
+	}
 
-    // Write to the standard output
-    fwrite = write(STDOUT_FILENO, buffer, fread);
-    if (fwrite == -1)
-    {
-        free(buffer);
-        close(file);
-        return (0);
-    }
+	fwrite = write(STDOUT_FILENO, buffer, fread);
+	if (fwrite == -1 || fwrite != fread)
+	{
+		free(buffer);
+		close(file);
+		return (0);
+	}
 
-    // Clean up and return the number of bytes written
-    free(buffer);
-    close(file);
-    return (fwrite);
+	free(buffer);
+	close(file);
+
+	return (fwrite);
 }
